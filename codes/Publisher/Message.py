b581de82e28c0ee3de60010f4782091d5eaeb82f
pil931032 <pil931032@gmail.com>
2024-04-06 14:19:04 +0000

    connecting function works

--- codes/Publisher/Message.py
+++ codes/Publisher/Message.py
@@ -20,7 +20,7 @@ class Message:
         plain_text_message = dict()
         plain_text_message['CPU_Temperature'] = cpu.temperature
         plain_text_message['CPU_Usage'] = psutil.cpu_percent()
-        plain_text_message['RAM_Usage'] = psutil.virtual_memory().percent
+        plain_text_message['RAM_Usage'] = psutil.virtual_memory().percent   
         plain_text = json.dumps(plain_text_message)
         
         encryption = Encryption()
